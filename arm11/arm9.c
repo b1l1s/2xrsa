ab8df961a2126cadcb44c46b41d8cb7da0d5d4ed
bilis <diefaust@gmail.com>
2016-02-16 08:40:31 +0800

    Basic OTP dumping, temp code

--- arm11/arm9.c
+++ arm11/arm9.c
@@ -22,6 +22,13 @@ void __attribute__ ((section (".text.a9"))) xmemcpy(void* dst, const void* src, 
 	}
 }
 
+void __attribute__ ((section (".text.a9"))) xmemset(void* addr, int val, unsigned int size)
+{
+	char* caddr = (char*) addr;
+	while(size--)
+		*(caddr++) = val;
+}
+
 void __attribute__ ((section (".text.a9"), naked)) svcBackdoor(void* funcptr)
 {
 	asm volatile
@@ -47,6 +54,35 @@ void __attribute__ ((section (".text.a9"))) mpuPerm()
 	);
 }
 
+typedef struct IFILE
+{
+	uint32_t unk[4];
+	uint64_t fptr;
+	uint64_t size;
+} IFILE;
+
+void __attribute__ ((section (".text.a9"))) data_dump()
+{
+	int(*IFile_Open)(IFILE* file, const wchar_t* path, uint32_t flags) = (void*)0x08065AA5;
+	void(*IFile_Write)(IFILE* file, uint32_t* written, const void* buffer, uint32_t size, uint32_t blockSize) = (void*)0x08068719;
+	
+	uint32_t tname[7];
+	tname[0] = 0x00640073;
+	tname[1] = 0x0063006D;
+	tname[2] = 0x002F003A;
+	tname[3] = 0x00390061;
+	tname[4] = 0x002E0066;
+	tname[5] = 0x00690062;
+	tname[6] = 0x0000006E;
+	
+	IFILE file;
+	uint32_t written;
+	
+	xmemset(&file, 0, sizeof(file));
+	IFile_Open(&file, (const wchar_t*)tname, 6);
+	IFile_Write(&file, &written, (const uint32_t*)0x10012000, 0x00000100, 1);
+}
+
 void __attribute__ ((section (".text.a9"))) core_main(void)
 {
 	uint32_t *cur = (uint32_t *)0x20000000;
@@ -57,11 +93,13 @@ void __attribute__ ((section (".text.a9"))) core_main(void)
 		"bl svcBackdoor\n\t"
 		:::"r0"
 	);
-
-	for (int i = 0; i < 0x00038400; ++i) {
-			((uint8_t*)0x1848F000)[i] = 0x01;
-			((uint8_t*)0x184C7800)[i] = 0x01;
-	}
+	
+	asm volatile
+	(
+		"adr r0, data_dump\n\t"
+		"bl svcBackdoor\n\t"
+		:::"r0"
+	);
 
 	do {
 		/* "FUCK" */
@@ -71,7 +109,17 @@ void __attribute__ ((section (".text.a9"))) core_main(void)
 
 	if ((uintptr_t)cur == (uintptr_t)0x28000000) {
 		/* Not found, let's just hang ourselves. */
-		while(1);
+		while(1)
+		for (int i = 0; i < 0x00038400; ++i) {
+			((uint8_t*)0x1848F000)[i] = 0x00;
+			((uint8_t*)0x184C7800)[i] = 0x00;
+		}
+	}
+	
+	while(1)
+	for (int i = 0; i < 0x00038400; ++i) {
+		((uint8_t*)0x1848F000)[i] = 0xFF;
+		((uint8_t*)0x184C7800)[i] = 0xFF;
 	}
 
 	/* Due to the post-increment in the loop above, "cur" already points
