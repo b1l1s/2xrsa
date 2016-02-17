3d16ad5a74f884977ebf5a6e96f014340d8959bc
bilis <diefaust@gmail.com>
2016-02-17 17:28:32 +0800

    Made compatible with Decrypt9(modified to use hardcoded framebuffer, tested only on 4.x)

--- arm11/arm9.c
+++ arm11/arm9.c
@@ -11,6 +11,15 @@ int __attribute__ ((section (".text.a9.entry"), naked)) core_start()
 	asm volatile ("bx lr");
 }
 
+void __attribute__ ((section (".text.a9"), naked)) svcSleepThread9(uint32_t nanoSecsLo, uint32_t nanoSecsHi)
+{
+	asm
+	(
+		"svc 0x0A\n\t"
+		"bx lr"
+	);
+}
+
 /* Slow but safe, from CakesROPSpider */
 void __attribute__ ((section (".text.a9"))) xmemcpy(void* dst, const void* src, uint32_t size)
 {
@@ -59,6 +68,61 @@ void __attribute__ ((section (".text.a9"))) mpuPerm()
 	);
 }
 
+void __attribute__ ((section (".text.a9"))) flushCache()
+{
+	asm
+	(
+		"mov r0, #0\n\t"
+		"mcr p15, 0, r0, c7, c5, 0\n\t"// Flush instruction cache
+		"mov r1, #0\n"
+	"flushloop0:\n\t"
+		"mov r0, #0\n"
+	"flushloop1:\n\t"
+		"orr r2, r1, r0\n\t"
+		"mcr p15, 0, r2, c7, c14, 2\n\t"// Clean and flush data cache entry (index and segment)
+		"add r0, r0, #0x20\n\t"			// Increment line
+		"cmp r0, #0x400\n\t"
+		"bne flushloop1\n\t"
+		"add r1, r1, #0x40000000\n\t"	// Increment segment counter
+		"cmp r1, #0\n\t"
+		"bne flushloop0\n\t"
+		"bx lr"
+		:::"r0","r1","r2", "memory"
+	);
+}
+
+void __attribute__ ((section (".text.a9"))) nop_sub()
+{
+}
+
+void __attribute__ ((section (".text.a9"))) disableInterrupts()
+{
+	asm
+	(
+		"mrs r1, cpsr\n\t"
+		"orr r1, r1, #0xC0\n\t" // Disable FIQ and IRQ interrupts
+		"msr cpsr_c, r1\n\t"
+		:::"r1"
+	);
+	
+	register uint32_t nopsubptr asm ("r0") = 0;
+	asm
+	(
+		"adr r0, nop_sub\n\t"
+		:::
+	);
+	
+	// nop the interrupt vector
+	uint32_t* v0 = (uint32_t*)0x8000000;
+	v0[0x01] = nopsubptr;
+	v0[0x05] = nopsubptr; // svc
+	v0[0x07] = nopsubptr;
+	v0[0x09] = nopsubptr;
+	v0[0x0B] = nopsubptr;
+	
+	flushCache();
+}
+
 typedef struct IFILE
 {
 	uint32_t unk[4];
@@ -88,8 +152,26 @@ void __attribute__ ((section (".text.a9"))) data_dump()
 	IFile_Write(&file, &written, (const uint32_t*)0x23F00000, 0x20000, 1);
 }
 
+void __attribute__ ((section (".text.a9"))) jump(void)
+{
+	disableInterrupts();
+	((void (*)())0x23F00000)();
+}
+
 void __attribute__ ((section (".text.a9"))) core_main(void)
 {
+	asm volatile
+	(
+		"adr r0, flushCache\n\t"
+		"bl svcBackdoor\n\t"
+		:::"r0"
+	);
+	uint32_t* single = (uint32_t*)0x23FFFE0C;
+	if(*single == 0xAABBCCDD)
+		while(1)
+			svcSleepThread9(0x1DCD6500, 0);
+	*single = 0xAABBCCDD;
+	
 	uint32_t *cur = (uint32_t *)0x20000000;
 	
 	asm volatile
@@ -115,6 +197,8 @@ void __attribute__ ((section (".text.a9"))) core_main(void)
 			((uint8_t*)0x184C7800)[i] = 0x00;
 		}
 	}
+	
+	//svcSleepThread9(0x1DCD6500, 0);
 
 	/* Size is completely arbitrary */
 	// Find and assign the fb addresses
@@ -126,5 +210,10 @@ void __attribute__ ((section (".text.a9"))) core_main(void)
 	*((unsigned int*)0x080FFFD4) = cur[2];
 	*((unsigned int*)0x080FFFD8) = 0;*/
 
-	svcBackdoor((void (*)())0x23F00000);
+	asm volatile
+	(
+		"adr r0, jump\n\t"
+		"bl svcBackdoor\n\t"
+		:::"r0"
+	);
 }
